5332cdcb25deaffa99bebf5708b8052ec7267b8b
daniel20max <51431192+daniel20max@users.noreply.github.com>
2021-11-18 23:30:25 -0300

    create atualizado

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded());
 
 const Anime =  require("./models/anime");
+const message = "";
 
 app.get("/", async (req, res) => {
     const animes = await Anime.findAll();
@@ -26,20 +27,46 @@ app.get("/anime/:id", async (req, res) => {
 });
 
 app.get("/animeCadastro", (req, res) => {
-    res.render("animeCadastro");
+    res.render("animeCadastro", {
+        message
+    });
 });
 
 app.post("/animeCadastrar", async (req, res) => {
-    const anime = await FileSystem.Create({
-        nome,
-        descricao,
-        imagem,
-   });
-   const id = Anime.findbyID(anime.id === Anime.id)
-   res.redirect("animeDetails", {
-       id,
-   })
-});
+    const { nome, descricao, imagem } = req.body;
+
+    if (!nome) {
+      res.render("animeCadastro", {
+        message: "Nome é obrigatório",
+      });
+    }
+  
+    else if (!imagem) {
+      res.render("animeCadastro", {
+        message: "Imagem é obrigatório",
+      });
+    }
+  
+    else {
+      try {
+        const anime = await Anime.create({
+          nome,
+          descricao,
+          imagem,
+        });
+  
+        res.render("animeCadastro", {
+            anime, message: "Seu Anime foi cadastrado!"
+        });
+      } catch (err) {
+        console.log(err);
+  
+        res.render("animeCadastro", {
+          message: "Ocorreu um erro ao cadastrar o Anime!",
+        });
+      }
+    }
+  });
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
